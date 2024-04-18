63c6192819608a1bb56d742da0901af86dce353d
akesh-0909 <akeshkumar65885@gmail.com>
2024-04-18 15:36:08 +0530

    dvc Iitialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is personal project in interset to learn MLOps',
+    author='akesh kumar',
+    license='MIT',
+)
